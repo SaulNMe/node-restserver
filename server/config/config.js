a2280c3594478efbac2a5677d82a3300b92341d4
SaulNMe <amrs1399@gmail.com>
2020-02-27 13:08:44 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/Cafeteria';
 }
 else {
-    urlDB = 'mongodb+srv://loquillonme:3xFJv91H3ZH27Bre@cluster0-8hkl0.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
